1e2cf0af333767a75a36216f1ed1e3385ed7a332
kaikaci12 <somewonesomewone@gmail.com>
2025-02-01 19:59:08 +0400

    add login,logout,register fuctions

--- context/authContext.js
+++ context/authContext.js
@@ -1,2 +1,27 @@
-import { createContext } from "react";
+import { createContext, useEffect } from "react";
 export const AuthContext = createContext();
+export const AuthContextProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [isAuthenticated, setIsAuthenticated] = useState(undefined);
+  useEffect(() => {
+    // onAuthStateChanged is a firebase method that listens for changes in the authentication state
+  });
+  const login = (email, password) => {
+    try {
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const logOut = () => {
+    try {
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const register = () => {
+    try {
+    } catch (error) {
+      console.log(error);
+    }
+  };
+};
